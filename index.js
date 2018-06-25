725e1b86140eaf82144e45e8ac63de16c554e366
Oskar Schöldström <m@oxy.fi>
2018-06-25 13:17:46 -0300

    quotes and new stylelint rules

--- index.js
+++ index.js
@@ -6,39 +6,43 @@ module.exports = {
     'stylelint-scss'
   ],
   rules: {
-    "no-empty-source": null,
-    "at-rule-empty-line-before": [
-      "always",
+    'no-empty-source': null,
+    'no-descending-specificity': null,
+    'no-duplicate-selectors': null,
+    'no-duplicate-at-import-rules': null,
+    'font-family-no-missing-generic-family-keyword': null,
+    'at-rule-empty-line-before': [
+      'always',
       {
-        "ignoreAtRules": [
-          "else"
+        'ignoreAtRules': [
+          'else'
         ],
-        "ignore": [
-          "after-comment"
+        'ignore': [
+          'after-comment'
         ],
-        "except": [
-          "first-nested",
-          "blockless-after-same-name-blockless"
+        'except': [
+          'first-nested',
+          'blockless-after-same-name-blockless'
         ]
       }
     ],
-    "block-opening-brace-space-before": "always",
-    "block-closing-brace-newline-after": [
-      "always",
+    'block-opening-brace-space-before': 'always',
+    'block-closing-brace-newline-after': [
+      'always',
       {
-        "ignoreAtRules": [
-          "if",
-          "else"
+        'ignoreAtRules': [
+          'if',
+          'else'
         ]
       }
     ],
-    "at-rule-name-space-after": "always",
-    "at-rule-no-unknown": null,
-    "scss/at-rule-no-unknown": true,
-    "scss/at-else-closing-brace-newline-after": "always-last-in-chain",
-    "scss/at-else-closing-brace-space-after": "always-intermediate",
-    "scss/at-else-empty-line-before": "never",
-    "scss/at-if-closing-brace-newline-after": "always-last-in-chain",
-    "scss/at-if-closing-brace-space-after": "always-intermediate"
+    'at-rule-name-space-after': 'always',
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+    'scss/at-else-closing-brace-newline-after': 'always-last-in-chain',
+    'scss/at-else-closing-brace-space-after': 'always-intermediate',
+    'scss/at-else-empty-line-before': 'never',
+    'scss/at-if-closing-brace-newline-after': 'always-last-in-chain',
+    'scss/at-if-closing-brace-space-after': 'always-intermediate'
   }
 };
